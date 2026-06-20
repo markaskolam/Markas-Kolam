b527478fea3832aa8541cb9393fea0e977af26da
markaskolam <markaskolam.com@gmail.com>
2026-06-20 20:10:24 +0700

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -7,5 +7,5 @@ export const isSupabaseConfigured = Boolean(supabaseUrl && supabaseAnonKey);
 
 export const supabase = createClient(
   supabaseUrl || 'https://estepabokizguiozhtoi.supabase.co',
-  supabaseAnonKey || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzdGVwYWJva2l6Z3Vpb3podG9pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NTEyMjksImV4cCI6MjA5NzUyNzIyOX0.xgTdl3-OLaS1Fyfc8ZVTnxWs4KVpNOrayph8X0dGCcw',
+  supabaseAnonKey || 'sb_publishable_agGS-2_lB9TmgFXtw0P1Cg_6agKc8H3',
 );
